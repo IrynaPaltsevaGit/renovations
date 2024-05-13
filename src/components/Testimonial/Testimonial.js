bbffc64af2e67c962e450e1b8e43fb6ab14cb884
Paltsevaleksand <paltsevaleksandr21@gmail.com>
2024-05-12 23:59:44 -0400

    implemented testimonials component with mobile and desktop styles

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faStar } from '@fortawesome/free-solid-svg-icons';
+import "./Testimonial.scss";
 
 
 export default function Testimonial({testimonial}) {
-  return (
-    <div>
-        <FontAwesomeIcon icon={ faUser } />
-        <h6>
-            { testimonial.date }
-        </h6>
-        <h5>
-            { testimonial.username }
-        </h5>
-        <p>
-            {testimonial.text }
-        </p>
-    </div>
-  )
+    const starArr = new Array(5).fill(0);
+
+    return (
+        <div className='testimonial'>
+            <div className='testimonial__header'>
+                
+
+                <div className='testimonial__header-left'>
+                    <h6 className='testimonial__date'>
+                        { testimonial.date }
+                    </h6>
+                    <h5 className='testimonial__name'>
+                        { testimonial.username }
+                    </h5>
+                </div>
+                <div className='testimonial__header-right'>
+                    <div className='testimonial__rate'>
+                    {
+                        starArr.map((item, ind) => {
+                            return (
+                                <FontAwesomeIcon icon={faStar} 
+                                    className='testimonial__star' 
+                                    key={`star-${ind}`} />
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+
+            </div>
+            
+            <p className='testimonial__text'>
+                { testimonial.text }
+            </p>
+            
+        </div>
+    )
 }
--- src/components/Testimonials/Testimonials.js
+++ src/components/Testimonials/Testimonials.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import Testimonial from '../Testimonial/Testimonial';
+import "./Testimonials.scss";
 
 export default function Testimonials() {
     const testimonials = [
         {
-            "username": "Nick",
-            "text": "text",
+            "username": "Iryna Paltseva",
+            "text": "Perfect work",
             "date": "May 11, 2024"
         },
         {
-            "username": "Nick1",
-            "text": "text",
+            "username": "Oleksandr",
+            "text": "Lorem ipsum dolor sit amet consectetur.",
             "date": "Feb 21, 2024"
         },
         {
-            "username": "Nick2",
-            "text": "text",
+            "username": "Nancy",
+            "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quidem aperiam reiciendis provident maiores nemo earum fugiat debitis vitae? Vitae.",
             "date": "Sep 08, 2024"
         },
     ];
   return (
     <section className='testimonials'>
+        <h2 className='testimonials__title'>
+            Testimonials
+        </h2>
         <div className='testimonials-wrap'>
             {
                 testimonials.map((testimonial, index) => {
@@ -33,6 +37,9 @@ export default function Testimonials() {
                 })
             }
         </div>
+        <a className='testimonials__link' href="#">
+            Leave us a review
+        </a>
     </section>
   )
 }
